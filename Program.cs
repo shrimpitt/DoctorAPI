2519ec2cf27725a596f6576a3b9b495fe20ce9bf
SanaBagym <bagymsana@gmail.com>
2026-03-28 00:35:54 +0500

    fix: update docker backend and admin auth integration

--- Program.cs
+++ Program.cs
@@ -109,3 +109,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
